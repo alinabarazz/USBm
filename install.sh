0b7b2abeb2cb12b5343dfa731365d67c56e4910b
Nino <nma.tarzona2@gmail.com>
2021-10-08 00:46:41 +0800

    update for linux

---
+++ install.sh
@@ -0,0 +1,32 @@
+@echo off
+ECHO Bot modified by virgaux(based on bot by PC jones) - have fun
+ECHO Press any key to start the installation
+PAUSE
+call npm install
+call npm update
+cd node_modules/puppeteer
+if exist .local-chromium (
+  cd ..\..\ 
+) else (
+  call node install
+cd ..\..\ 
+)
+ECHO.
+ECHO Installation complete!
+ECHO.
+:start
+SET choice=
+SET /p choice=Want to start the bot now? [Y/N]: 
+IF NOT '%choice%'=='' SET choice=%choice:~0,1%
+IF /i '%choice%'=='Y' GOTO yes
+IF /i '%choice%'=='N' GOTO no
+ECHO "%choice%" is not valid
+ECHO.
+GOTO start
+
+:no
+EXIT
+
+:yes
+ECHO Starting bot - have fun!
+call start.sh
---
+++ start.sh
@@ -0,0 +1,8 @@
+@echo off
+echo --------------------------------------------------------------------------
+echo Ultimate Splinderlands Bot Mobile by Virgaux(Based on bot by PC jones - V1)
+echo Join the telegram group https://t.me/ultimatesplinterlandsbot
+echo Join the discord server https://discord.gg/hwSr7KNGs9
+echo                    Close this window to stop the bot
+echo --------------------------------------------------------------------------
+node --max-old-space-size=8192 index.js
