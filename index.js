1b2b8dafb82d04672ef2a833bf8e7c19a912c8b6
Nino <nma.tarzona2@gmail.com>
2021-10-13 03:43:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -782,8 +782,7 @@ const sleepingTime = sleepingTimeInMinutes * 60000;
         envStatus.push('Telegram Notification: ' + teleNotif);
         envStatus.push('Use API: ' + useAPI);
         envStatus.push('Accounts: ' + chalk.greenBright(accounts));
-        const os = require("os"); // Comes with node.js
-        console.log(os.platform());
+        console.log(process.platform)
 
         if (process.env.TELEGRAM_NOTIF === 'true') { 
             await tn.tbotResponse(envStatus)
